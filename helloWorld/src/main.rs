5e6f9cc27a0c87a8b5f5901757d8174d55fadea4
scd7896 <scd1212@naver.com>
2019-12-09 19:41:05 +0900

    first rust

---
+++ helloWorld/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+use rand::Rng;
+
+fn main(){
+    println!("숫자를 입력해보고");
+    println!("스트링도 입력해보고");
+    let secret_number = rand::thread_rng().gen_range(1,10);
+    println!("비밀번호 : {}", secret_number);
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("라인읽기 실패");
+    println!("입력한 스트링 값 : {}", guess);
+
+}
